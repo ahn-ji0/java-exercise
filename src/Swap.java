af98130201031935642c75e1e8ca1b4990ca2f67
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-05 09:34:15 +0900

    modified commit

--- src/Swap.java
+++ src/Swap.java
@@ -1,8 +1,14 @@
 public class Swap {
-    public static void main(String[] args) {
-        int a,b;
 
-        
+    public static void main(String[] args) {
+        int a =10;
+        int b = 10;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
 
     }
 }
