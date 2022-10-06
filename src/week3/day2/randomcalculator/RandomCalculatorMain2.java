e71c502efb1dc5cad082efebfe89d0fe123bab41
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-06 09:13:26 +0900

    randomcalculator with basenum

--- src/week3/day2/randomcalculator/RandomCalculatorMain2.java
+++ src/week3/day2/randomcalculator/RandomCalculatorMain2.java
@@ -3,8 +3,10 @@ package week3.day2.randomcalculator;
 public class RandomCalculatorMain2{
 
     public static void main(String[] args) {
-        NumberGenerator numberMaker = new RandomNumberGenerator();
-        RandomCalculator2 randomCalculator2 = new RandomCalculator2(numberMaker);
+        // 부모 = 자식 (다형성 적용)
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        RandomCalculator2 randomCalculator2 = new RandomCalculator2(numberGenerator);
         randomCalculator2.plus(30);
     }
 }
