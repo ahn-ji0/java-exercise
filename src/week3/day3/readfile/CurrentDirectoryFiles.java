5a06bf043249d942fbaa57586f2d7a012dbd4a82
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-06 15:12:39 +0900

    get current directory files

---
+++ src/week3/day3/readfile/CurrentDirectoryFiles.java
@@ -0,0 +1,13 @@
+package week3.day3.readfile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class CurrentDirectoryFiles {
+    public static void main(String[] args) throws FileNotFoundException {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for(File file : files)
+            System.out.println(file);
+    }
+}
