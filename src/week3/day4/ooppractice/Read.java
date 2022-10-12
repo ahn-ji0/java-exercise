10a0560580826618688e1b5aadd885cc1b5ea3db
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-12 20:28:54 +0900

    read 수정

--- src/week3/day4/ooppractice/Read.java
+++ src/week3/day4/ooppractice/Read.java
@@ -10,16 +10,24 @@ import java.util.List;
 
 public class Read<T> {
     private Parse<T> parse;
+    boolean removeColumnName = false;
 
     public Read(Parse<T> parse){
         this.parse = parse;
     }
 
+    public Read(Parse<T> parse, boolean removeColumnName){
+        this.parse = parse;
+        this.removeColumnName = removeColumnName;
+    }
     public List<T> readLines(String fileName) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         List<T> myList = new ArrayList<>();
         String line;
 
+        if(removeColumnName){
+            br.readLine();
+        }
         while ((line = br.readLine()) != null) {
             T tmp = parse.parsing(line);
             myList.add(tmp);
