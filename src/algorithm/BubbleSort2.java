576eb652b37bf9019a4905190b462bb103ce1988
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-13 10:19:35 +0900

    bubble sort2

--- src/algorithm/BubbleSort2.java
+++ src/algorithm/BubbleSort2.java
@@ -3,6 +3,7 @@ package algorithm;
 import java.util.Arrays;
 
 public class BubbleSort2 {
+
     public int[] sort(int[] arr){
         for (int i=0; i<arr.length-1;i++){
             for(int j=i+1; j<arr.length-1; j++){
